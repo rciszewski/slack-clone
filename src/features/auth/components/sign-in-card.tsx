6508fb08e1fa852db9b4674313776d4d0e8434fe
Ron <rc72095@icloud.com>
2024-12-24 18:17:05 -0500

    add shadcn, fix tailwind config issues, increment sign in, and add bun as package manager

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,60 @@
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
+export const SignInCard = () => {
+  return (
+    <Card className="p-8 w-full h-full">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Login to continue</CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size={"lg"} disabled={false}>
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size={"lg"}
+            className="w-full relative flex items-center"
+          >
+            <FcGoogle className="absolute left-2.5 top-2.5 size-5" />
+            <span className="w-full">Continue with Google</span>
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
