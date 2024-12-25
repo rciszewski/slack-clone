94d3a856f264892ec5858b3d25cbc3d478705385
Ron <rc72095@icloud.com>
2024-12-25 15:51:54 -0500

    add full name input for sign up and fix redirect bug

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,5 +1,8 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+
+import { useAuthActions } from "@convex-dev/auth/react";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,6 +13,8 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { TriangleAlert } from "lucide-react";
+
 import { SignInFlow } from "../types";
 import { useState } from "react";
 
@@ -17,10 +22,39 @@ interface SignUpCardProps {
   setState: (state: SignInFlow) => void;
 }
 
-export const SignUpCard = ({setState}: SignUpCardProps) => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
+export const SignUpCard = ({ setState }: SignUpCardProps) => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState("");
+
+  const { signIn } = useAuthActions();
+
+  const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    setPending(true);
+    signIn("password", { name, email, password, flow: "signUp" })
+      .catch(() => {
+        setError("Something went wrong");
+      })
+      .finally(() => {
+        setPending(false);
+      });
+  };
+
+  const onProviderSignUp = (value: "github" | "google") => {
+    setPending(true);
+    signIn(value).finally(() => {
+      setPending(false);
+    });
+  };
 
   return (
     <Card className="p-8 w-full h-full">
@@ -30,41 +64,69 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4" />
+          <p>{error}</p>
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form onSubmit={onPasswordSignUp} className="space-y-2.5">
           <Input
-            disabled={false}
+            disabled={pending}
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder="Full name"
+            required
+          />
+          <Input
+            disabled={pending}
             value={email}
-            onChange={(e) => {setEmail(e.target.value)}}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             placeholder="Email"
             type="email"
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={password}
-            onChange={(e) => {setPassword(e.target.value)}}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             placeholder="Confirm password"
             type="password"
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={confirmPassword}
-            onChange={(e) => {setConfirmPassword(e.target.value)}}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
             placeholder="Password"
             type="password"
             required
           />
-          <Button type="submit" className="w-full" size={"lg"} disabled={false}>
+          <Button
+            type="submit"
+            className="w-full"
+            size={"lg"}
+            disabled={pending}
+          >
             Continue
           </Button>
         </form>
         <Separator />
         <div className="flex flex-col gap-y-2.5">
           <Button
-            disabled={false}
-            onClick={() => {}}
+            disabled={pending}
+            onClick={() => {
+              onProviderSignUp("google");
+            }}
             variant="outline"
             size={"lg"}
             className="w-full relative"
@@ -73,8 +135,10 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
             Continue with Google
           </Button>
           <Button
-            disabled={false}
-            onClick={() => {}}
+            disabled={pending}
+            onClick={() => {
+              onProviderSignUp("github");
+            }}
             variant="outline"
             size={"lg"}
             className="w-full relative"
@@ -85,7 +149,10 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
         </div>
         <p className="text-xs text-muted-foreground">
           Alreadyhave an account?{" "}
-          <span onClick={() => setState("signIn")} className="text-sky-700 hover:underline cursor-pointer">
+          <span
+            onClick={() => setState("signIn")}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
             Sign in
           </span>
         </p>
@@ -93,4 +160,3 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
     </Card>
   );
 };
-
--- src/middleware.ts
+++ src/middleware.ts
@@ -8,16 +8,18 @@ const isPublicPage = createRouteMatcher(["/auth"]);
 
 export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
   const isAuthenticated = await convexAuth.isAuthenticated();
+
   //private page and not authenticated -> auth
   if (!isPublicPage(request) && !isAuthenticated) {
     return nextjsMiddlewareRedirect(request, "/auth");
   }
 
-  if(isPublicPage(request) && isAuthenticated){
+  if (isPublicPage(request) && isAuthenticated) {
+    console.log(request);
     return nextjsMiddlewareRedirect(request, "/");
   }
   //TODO: Redirect user away from "/auth" if authenticated
- });
+});
 
 export const config = {
   // The following matcher runs middleware on all routes
