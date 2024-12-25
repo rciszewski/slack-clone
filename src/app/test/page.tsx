8edc5f18175e0cc67c8fb35dae218958be888476
Ron <rc72095@icloud.com>
2024-12-25 10:47:49 -0500

    setup middleware for public routes and redirect

---
+++ src/app/test/page.tsx
@@ -0,0 +1,6 @@
+const TestPage = () => {
+  return (
+    <div>Test</div>
+  )
+}
+export default TestPage
---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/auth"]);
+
+export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+  const isAuthenticated = await convexAuth.isAuthenticated();
+  //private page and not authenticated -> auth
+  if (!isPublicPage(request) && !isAuthenticated) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+
+  //TODO: Redirect user away from "/auth" if authenticated
+ });
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
