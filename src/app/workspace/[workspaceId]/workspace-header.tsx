954697260b2dbf1d81825276d8d0a0c034df7b06
Ron <rc72095@icloud.com>
2025-01-01 17:29:13 -0500

    fix member api endpoint to get unique member and update worksapce sidebar and header components

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -1,6 +1,63 @@
-const WorkspaceHeader = () => {
-  return (
-    <div>WorkspaceHeader</div>
-  )
+import { Button } from "@/components/ui/button";
+import { Doc } from "../../../../convex/_generated/dataModel";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+
+interface WorkspaceHeaderProps {
+  workspace: Doc<"workspaces">;
+  isAdmin: boolean;
 }
-export default WorkspaceHeader
+
+const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
+  return (
+    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant={"transparent"}
+            className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+            size={"sm"}
+          >
+            <span className="truncate">{workspace.name}</span>
+            <ChevronDown className="size-4 ml-1 shrink-0" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="bottom" align="start" className="w-64">
+          <DropdownMenuItem className="cursor-pointer capitalize">
+            <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold rounded-md text-xl flex items-center justify-center mr-2">
+              {workspace.name.charAt(0).toUpperCase()}
+            </div>
+            <div className="flex flex-col items-start">
+              <span className="font-bold">{workspace.name}</span>
+              <span className="text-xs text-muted-foreground">
+                Active workspace
+              </span>
+            </div>
+          </DropdownMenuItem>
+          {isAdmin && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="cursor-pointer py-2"
+                onClick={() => {}}
+              >
+                Invite people to {workspace.name}
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer py-2">
+                Preferences
+              </DropdownMenuItem>
+            </>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+export default WorkspaceHeader;
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -31,7 +31,10 @@ const WorkspaceSidebar = () => {
 
   return (
     <div className="flex flex-col bg-[#5e2c5f] h-full">
-      <WorkspaceHeader />
+      <WorkspaceHeader
+        workspace={workspace}
+        isAdmin={member.role === "admin"}
+      />
     </div>
   );
 };
