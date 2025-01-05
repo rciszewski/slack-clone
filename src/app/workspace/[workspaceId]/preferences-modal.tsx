a7f58e1fb7a8368dce0c383a71b346d247e7d79e
Ron <rc72095@icloud.com>
2025-01-05 17:30:55 -0500

    add api for update and remove workspaces, add to preferences modal

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -1,4 +1,11 @@
 import { useState } from "react";
+import { TrashIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { useUpdateWorkSpace } from "@/features/workspaces/api/use-update-workspace";
+import { useRemoveWorkSpace } from "@/features/workspaces/api/use-remove-workspace";
+import { useWorkSpaceId } from "@/hooks/use-workspace-id";
 
 import {
   Dialog,
@@ -12,7 +19,8 @@ import {
   DialogTitle,
   DialogDescription,
 } from "@/components/ui/dialog";
-import { TrashIcon } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface PreferencesModalProps {
   open: boolean;
@@ -26,6 +34,48 @@ const PreferencesModal = ({
   initialValue,
 }: PreferencesModalProps) => {
   const [value, setValue] = useState(initialValue);
+  const [editOpen, setEditOpen] = useState(false);
+
+  const workspaceId = useWorkSpaceId();
+
+  const router = useRouter();
+
+  const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } =
+    useUpdateWorkSpace();
+  const { mutate: removeWorkspace, isPending: isRemovingWorkspace } =
+    useRemoveWorkSpace();
+
+  const handleEdit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    updateWorkspace(
+      { id: workspaceId, name: value },
+      {
+        onSuccess: () => {
+          setEditOpen(false);
+          toast.success("Workspace updated");
+        },
+        onError: () => {
+          toast.error("Failed to update workspace");
+        },
+      }
+    );
+  };
+
+  const handleRemove = () => {
+    removeWorkspace(
+      { id: workspaceId },
+      {
+        onSuccess: () => {
+          router.replace("/");
+          toast.success("Workspace removed");
+        },
+        onError: () => {
+          toast.error("Failed to remove workspace");
+        },
+      }
+    );
+  };
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="p-0 bg-gray-50 overflow-hidden">
@@ -33,19 +83,48 @@ const PreferencesModal = ({
           <DialogTitle>{value}</DialogTitle>
         </DialogHeader>
         <div className="px-4 pb-4 flex flex-col gap-y-2">
-          <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
-            <div className="flex items-center justify-between">
-              <p className="text-sm font-semibold">Workspace name</p>
-              <p className="text-sm text-[#1264a3] hover:underline font-semibold">
-                Edit
-              </p>
-            </div>
-            <p className="text-sm">{value}</p>
-          </div>
+          <Dialog open={editOpen} onOpenChange={setEditOpen}>
+            <DialogTrigger asChild>
+              <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-semibold">Workspace name</p>
+                  <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                    Edit
+                  </p>
+                </div>
+                <p className="text-sm">{value}</p>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Rename this workspace</DialogTitle>
+              </DialogHeader>
+              <form className="space-y-4" onSubmit={handleEdit}>
+                <Input
+                  value={value}
+                  disabled={isUpdatingWorkspace}
+                  onChange={(e) => setValue(e.target.value)}
+                  required
+                  autoFocus
+                  minLength={3}
+                  maxLength={80}
+                  placeholder="Worksapce name e.g. 'Work', 'Personal', 'Home'"
+                />
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant={"outline"} disabled={isUpdatingWorkspace}>
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <Button disabled={isUpdatingWorkspace}>Save</Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
           <button
             className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
-            disabled={false}
-            onClick={() => {}}
+            disabled={isRemovingWorkspace}
+            onClick={handleRemove}
           >
             <TrashIcon className="size-4" />
             <p className="text-sm font-semibold">Delete workspace</p>
--- src/features/workspaces/api/use-update-workspace.ts
+++ src/features/workspaces/api/use-update-workspace.ts
@@ -3,7 +3,7 @@ import { api } from "../../../../convex/_generated/api";
 import { useCallback, useMemo, useState } from "react";
 import { Id } from "../../../../convex/_generated/dataModel";
 
-type RequestType = { name: string };
+type RequestType = {id: Id<"workspaces">, name:string};
 type ResponseType = Id<"workspaces"> | null;
 
 type Options = {
@@ -13,7 +13,7 @@ type Options = {
   throwError?: boolean;
 };
 
-export const useCreateWorkSpace = () => {
+export const useUpdateWorkSpace = () => {
   const [data, setData] = useState<ResponseType>(null);
   const [error, setError] = useState<Error | null>(null);
   const [status, setStatus] = useState<
@@ -25,7 +25,7 @@ export const useCreateWorkSpace = () => {
   const isPending = useMemo(() => status === "pending", [status]);
   const isSettled = useMemo(() => status === "settled", [status]);
 
-  const mutation = useMutation(api.workspaces.create);
+  const mutation = useMutation(api.workspaces.update);
 
   const mutate = useCallback(
     async (values: RequestType, options?: Options) => {
@@ -39,6 +39,7 @@ export const useCreateWorkSpace = () => {
         options?.onSuccess?.(response);
         return response;
       } catch (error) {
+        setStatus("error")
         options?.onError?.(error as Error);
         if (options?.throwError) {
           throw error;
