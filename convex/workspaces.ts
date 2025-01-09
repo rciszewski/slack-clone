01bed21cc9e66020c7a3f4d6408547e6d2b3d2c4
Ron <rc72095@icloud.com>
2025-01-09 06:18:39 -0500

    add api endpoint for joining a workspace

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -63,6 +63,45 @@ const generateCode = () => {
   return code;
 };
 
+export const join = mutation({
+  args: { workspaceId: v.id("workspaces"), joinCode: v.string() },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const workspace = await ctx.db.get(args.workspaceId);
+    if (!workspace) {
+      throw new Error("Workspace not found");
+    }
+
+    if (workspace.joinCode !== args.joinCode.toLowerCase()) {
+      throw new Error("Invalid join code");
+    }
+
+    const existingMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (existingMember) {
+      throw new Error("Already a member of this workspace");
+    }
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId: workspace._id,
+      role: "member",
+    });
+
+    return workspace._id;
+  },
+});
+
 export const newJoinCode = mutation({
   args: {
     workspaceId: v.id("workspaces"),
